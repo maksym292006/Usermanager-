69984b00216c31673c80e480fd7ef2caf293d849
Pedro Torres <ptorresgutierrez@iesmordefuentes.com>
2025-01-10 11:29:34 +0100

    Somos muy listos(Llevamos varios dias con esto XD)

--- UserManager.java
+++ UserManager.java
@@ -3,11 +3,10 @@ import java.util.List;
 
 public class UserManager {
     public static List<String> users = new ArrayList<>();
-    
-    public static void listUsers() {
-    for (String user : users) {
-        System.out.println(user);
+
+    public static void addUser(String user) {
+        if (!users.contains(user)) { // Verifica si el usuario ya existe
+            users.add(user);         // Si no existe, lo añade
+        }
     }
-}
-    
 }
