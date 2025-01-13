ad1fa3d0779ffb8ad580ca8eb8d9fc1ba4fe1f85
“maxym” <maxym@iesmordefuentes.com>
2025-01-13 13:46:39 +0100

    Agregado el método main para probar la funcionalidad de usuarios

--- UserManager.java
+++ UserManager.java
@@ -4,4 +4,20 @@ import java.util.List;
 public class UserManager {
     public static List<String> users = new ArrayList<>();
     
+
+public static void addUser(String user) {
+    if (!users.contains(user)) {
+        users.add(user);
+    }
 }
+
+public static void main(String[] args) {
+    addUser("Alice");
+    addUser("Bob");
+    listUsers();
+}
+
+}
+
+
+
